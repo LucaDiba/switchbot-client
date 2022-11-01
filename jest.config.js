12ce2b322e05bece9ae082e15ab8f6d1d4982944
Luca Dibattista <l.diba@protonmail.com>
2022-10-31 23:21:37 -0600

    Init project

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true,
+  coverageDirectory: "coverage",
+};

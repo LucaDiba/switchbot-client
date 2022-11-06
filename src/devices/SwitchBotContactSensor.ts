cc7b6617d2a33ff14a19f1da913d0b1911d6996e
Luca Dibattista <l.diba@protonmail.com>
2022-11-06 01:31:51 -0700

    Add SwitchBot Contact and Motion sensors

---
+++ src/devices/SwitchBotContactSensor.ts
@@ -0,0 +1,22 @@
+import {
+  BaseDeviceGetDeviceBody,
+  BaseDeviceWithMotionBrightnessStatusBody,
+} from "../types.js";
+import { DEVICE_TYPES } from "../utils/constant.js";
+import { Device } from "./Device.js";
+
+type DeviceType = typeof DEVICE_TYPES.CONTACT_SENSOR;
+
+export type GetDeviceBody = BaseDeviceGetDeviceBody<DeviceType>;
+
+export type StatusBody =
+  BaseDeviceWithMotionBrightnessStatusBody<DeviceType> & {
+    openState: "open" | "close" | "timeOutNotClose";
+  };
+
+export type CommandBody = never;
+
+export default class SwitchBotContactSensor extends Device<
+  StatusBody,
+  CommandBody
+> {}

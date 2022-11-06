f2c1446b0c4f283389d8d863903c87186296edf2
Luca Dibattista <l.diba@protonmail.com>
2022-11-05 23:15:45 -0600

    Fix types in SwitchBot Plug

--- src/devices/SwitchBotPlug.ts
+++ src/devices/SwitchBotPlug.ts
@@ -1,17 +1,23 @@
-import { BaseDeviceWithPowerStatusBody } from "../types.js";
+import {
+  BaseDeviceGetDeviceBody,
+  BaseDeviceWithPowerStatusBody,
+} from "../types.js";
 import { DEVICE_TYPES } from "../utils/constant.js";
 import { DeviceWithPower } from "./Device.js";
+
+type GetDeviceBody = BaseDeviceGetDeviceBody;
 
 type StatusBody = BaseDeviceWithPowerStatusBody & {
   deviceType: typeof DEVICE_TYPES.BOT;
 };
 
-type CommandBody = {};
+type CommandBody = any; // TODO: Figure out what this is
 
 export default class SwitchBotPlug extends DeviceWithPower<
   StatusBody,
   CommandBody
 > {}
 
+export type SwitchBotPlugGetDeviceBody = GetDeviceBody;
 export type SwitchBotPlugStatusBody = StatusBody;
 export type SwitchBotPlugCommandBody = CommandBody;
